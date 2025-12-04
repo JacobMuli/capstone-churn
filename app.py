fa4f22f944f05a05cd7ab815e38805885362169f
JacobMuli <148524545+JacobMuli@users.noreply.github.com>
2025-12-04 07:29:12 +0300

    Update MODEL_DIR path for model artifacts

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from tensorflow.keras.models import load_model
 # Load Artifacts (V6)
 # -----------------------------------------------------------
 
-MODEL_DIR = "/mnt/data/churn_model_v6"
+MODEL_DIR = "/mnt/data"
 
 preprocessor = joblib.load(f"{MODEL_DIR}/preprocessor.joblib")
 train_columns = joblib.load(f"{MODEL_DIR}/train_columns.joblib")
